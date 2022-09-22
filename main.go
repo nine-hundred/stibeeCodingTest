67a1a4e96bcf82104adab3374a41dcbff36c92d5
nine-hundred <ninehundred@outlook.kr>
2022-09-22 11:59:25 +0900

    fix: send stage start request
    생성한 요청을 보내기

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"stibee/controllers"
 )
@@ -18,10 +19,20 @@ func main() {
 func startStage(level string) error {
 	attackServerUrl := "http://attack.stibee.kr:8090/stage?id=ninehundred@outlook.kr"
 
-	_, err := http.NewRequest("POST", attackServerUrl+"&level="+level, nil)
+	req, err := http.NewRequest("POST", attackServerUrl+"&level="+level, nil)
 	if err != nil {
 		fmt.Printf("stage%s can't start", level)
 		return err
 	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	bytes, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(bytes))
+	fmt.Println("stage" + level + "====================")
 	return nil
 }
